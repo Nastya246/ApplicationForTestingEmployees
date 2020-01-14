8dce796d1b5fdb96262e965a45122bf6fa11f3fe
nastya nastya <nastya5551996@mail.ru>
2020-01-15 00:04:48 +0500

    законченно добавление вопросов

--- WebApplicationForTest/WebApplicationForTest/Controllers/ВопросыController.cs
+++ WebApplicationForTest/WebApplicationForTest/Controllers/ВопросыController.cs
@@ -8,7 +8,7 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using WebApplicationForTest.Models;
-
+using System.Data.Entity.Validation;
 namespace WebApplicationForTest.Controllers
 {
     public class ВопросыController : Controller
@@ -159,11 +159,15 @@ namespace WebApplicationForTest.Controllers
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> Create([Bind(Include = "id_вопроса,id_Теста,Текст_вопроса,Тип_ответа,Номер_подвопроса")] Вопросы вопросы, int id_Теста, string variant, string correct, string def)
         {
-            вопросы.id_Теста = id_Теста;
+            
+                вопросы.id_Теста = id_Теста;
+            
             if (ModelState.IsValid)
             {
-                Ответы ответы = new Ответы();               
+                try { 
+                Ответы ответы = new Ответы();
                 db.Вопросы.Add(вопросы);
+
                 await db.SaveChangesAsync();
 
                 if (вопросы.Тип_ответа.Replace(" ", "") == "Выбор")
@@ -189,7 +193,7 @@ namespace WebApplicationForTest.Controllers
                 }
                 else if (вопросы.Тип_ответа.Replace(" ", "") == "Несколько")
                 {
-                    string[] stringTempListVar = variant.Split(',');
+                    string[] stringTempListVar = variant.Split(','); //разделяем варианты ответов
                     var resulttemp = stringTempListVar.Where(x => !string.IsNullOrWhiteSpace(x)).ToArray();
                     stringTempListVar = resulttemp;
 
@@ -231,11 +235,108 @@ namespace WebApplicationForTest.Controllers
                     db.Ответы.Add(ответы);
                     await db.SaveChangesAsync();
                 }
-                    return RedirectToAction("Index", new { topic = вопросы.id_Теста.ToString(), redactor = "redactor" });
+                else if (вопросы.Тип_ответа.Replace(" ", "") == "Соотношение")
+                {
+
+                    string[] stringTempListCor = correct.Split(',');//разделяем корректные варианты ответов
+                    var resulttemp = stringTempListCor.Where(x => !string.IsNullOrWhiteSpace(x)).ToArray();
+                    stringTempListCor = resulttemp;
+
+                    string[] stringTempListDef = def.Split(',');//разделяем понятия
+                    resulttemp = stringTempListDef.Where(x => !string.IsNullOrWhiteSpace(x)).ToArray();
+                    stringTempListDef = resulttemp;
+                    int countDef = 1;
+                    int countCor = 0;
+
+
+                    foreach (var deftemp in stringTempListDef) //вносим понятия в бд
+                    {
+                        ответы.id_Вопроса = вопросы.id_вопроса;
+                        ответы.Текст_ответа = countDef.ToString() + " " + deftemp;
+                        ответы.Флаг_правильного_ответа = true;
+                        ответы.Флаг_подвопроса = true;
+                        ответы.Правильный_ответ = stringTempListCor[countCor];
+                        countCor++;
+                        countDef++;
+                        db.Ответы.Add(ответы);
+                        await db.SaveChangesAsync();
+                    }
+
+                    string[] stringTempListVar = stringTempListCor; //для вариантов ответов
+
+                    Random rand = new Random();
+                    for (int i = stringTempListVar.Length - 1; i >= 1; i--) //генерируем варианты ответов в случайном порядке
+                    {
+                        int j = rand.Next(i + 1);
+                        string tmp = stringTempListVar[j];
+                        stringTempListVar[j] = stringTempListVar[i];
+                        stringTempListVar[i] = tmp;
+                    }
+                    char countV = 'a';
+                    foreach (var vartemp in stringTempListVar) //вносим варианты ответов в бд
+                    {
+                        ответы.id_Вопроса = вопросы.id_вопроса;
+                        ответы.Текст_ответа = countV + " " + vartemp;
+                        ответы.Флаг_правильного_ответа = true;
+                        ответы.Флаг_подвопроса = false;
+                            ответы.Правильный_ответ = null;
+                        countV++;
+                        db.Ответы.Add(ответы);
+                        await db.SaveChangesAsync();
+                    }
+                }
+                else if (вопросы.Тип_ответа.Replace(" ", "") == "Разрыв")
+                {
+
+                    ответы.id_Вопроса = вопросы.id_вопроса;
+                    ответы.Текст_ответа = correct;
+                    ответы.Флаг_правильного_ответа = true;
+                    db.Ответы.Add(ответы);
+                    await db.SaveChangesAsync();
+
+                    string[] stringTempListVar = correct.Split(',');//подготавливаем для составления списка с вариантами ответов
+                    var resulttemp = stringTempListVar.Where(x => !string.IsNullOrWhiteSpace(x)).ToArray();
+                    stringTempListVar = resulttemp;
+                    Random rand = new Random();
+                    for (int i = stringTempListVar.Length - 1; i >= 1; i--) //генерируем варианты ответов в случайном порядке
+                    {
+                        int j = rand.Next(i + 1);
+                        string tmp = stringTempListVar[j];
+                        stringTempListVar[j] = stringTempListVar[i];
+                        stringTempListVar[i] = tmp;
+                    }
+
+                    foreach (var vartemp in stringTempListVar) //вносим варианты ответов в БД
+                    {
+                        ответы.id_Вопроса = вопросы.id_вопроса;
+                        ответы.Текст_ответа = vartemp;
+                        ответы.Флаг_правильного_ответа = false;
+
+                        db.Ответы.Add(ответы);
+                        await db.SaveChangesAsync();
+                    }
+
+                }
+                return RedirectToAction("Index", new { topic = вопросы.id_Теста.ToString(), redactor = "redactor" });
             }
-            
+            catch (DbEntityValidationException ex)
+
+                {
+                    foreach (DbEntityValidationResult validationError in ex.EntityValidationErrors)
+                    {
+                        Response.Write("Object: " + validationError.Entry.Entity.ToString());
+                        Response.Write(" ");
+                             foreach (DbValidationError err in validationError.ValidationErrors)
+                        {
+                            Response.Write(err.ErrorMessage + " ");
+                        }
+                    }
+                }
+
+                }
             ViewBag.id_Теста = new SelectList(db.Тесты, "id_теста", "Название_темы_теста", вопросы.id_Теста);
-            return View(вопросы);
+                return View(вопросы);
+           
         }
 
         // GET: Вопросы/Edit/5
